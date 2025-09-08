ef17c58818dbc2cd7a1d00b41ad304c5fc43b953
Harold1Maldonado <haroldmaldonado1@gmail.com>
2025-09-08 19:28:14 +0000

    creadas las funciones

---
+++ app.js
@@ -0,0 +1,51 @@
+
+const asciiHeart = [
+    "⣠⣤⣶⣶⣦⣄⡀  ⠀⢀⣤⣴⣶⣶⣤⣀",
+    "⣼⣿⣿⣿⣿⣿⣿⣷⣤⣾⣿⣿⣿⣿⣿⣿⣧",
+    "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿",
+    "⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏",
+    " ⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋ ",
+    "   ⠙⢿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠁⠀",
+    "     ⠉⢿⣿⣿⣿⠟⠋⠀",
+    "⠀      ⠙⠻⠁"
+];
+
+function printByLine(time) {
+    asciiHeart.forEach((line, index) => {
+        setTimeout(() => {
+            process.stdout.write(line + "\n");
+        }, index * time);
+    });
+}
+
+
+
+
+
+function printByChart(time) {
+    let delay = 0;
+
+    for (let i = 0; i < asciiHeart.length; i++) {
+        const chars = asciiHeart[i].split("");
+
+        chars.forEach((char, index) => {
+
+            setTimeout(() => {
+                process.stdout.write(char);
+                
+                if ( char.length === index) {
+                    console.log("")
+                }
+                
+            }, delay);
+            delay += time;
+        });
+
+
+
+
+    }
+}
+
+
+printByChart(50);
